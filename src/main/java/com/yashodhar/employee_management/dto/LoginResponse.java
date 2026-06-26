806d82ee709a0f773f9764bf4725d4adaacaa778
Yashodhar G K <yashodahrgk7@gmail.com>
2026-06-26 13:34:43 +0530

    Add JWT authentication and role-based authorization

---
+++ src/main/java/com/yashodhar/employee_management/dto/LoginResponse.java
@@ -0,0 +1,21 @@
+package com.yashodhar.employee_management.dto;
+
+public class LoginResponse {
+
+    private String token;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- src/main/java/com/yashodhar/employee_management/security/SecurityConfig.java
+++ src/main/java/com/yashodhar/employee_management/security/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.yashodhar.employee_management.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +23,14 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
+        return http
                 .csrf(csrf -> csrf.disable())
 
                 .headers(headers ->
@@ -31,7 +39,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
-                                .requestMatchers("/auth/register").permitAll()
+                                .requestMatchers("/auth/**").permitAll()
 
                                 .requestMatchers("/h2-console/**").permitAll()
 
@@ -50,9 +58,23 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                         )
                 
-                .httpBasic(Customizer.withDefaults()
-                );
+                .httpBasic(httpBasic -> httpBasic.disable())
 
-        return http.build();
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+
+                .build();
+        
     }
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+
+    }
+
 }
